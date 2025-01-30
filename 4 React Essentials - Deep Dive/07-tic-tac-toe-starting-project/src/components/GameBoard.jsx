13f0979c9b6ef611064eefc0f8d1b2c1dfa302e5
maxwsj <mw2study@gmail.com>
2025-01-29 23:43:24 -0300

    add final concepts from React Essentials - Deep dive: - Avoid Intersecting States! - Prefer Computed Values & Avoid Unnecessary State Management - Deriving State From Props - Sharing State Across Components - Reducing State Management & Identifying Unnecessary State - Disabling Buttons Conditionally - Outsourcing Data Into A Separate File - Lifting Computed Values Up - Deriving Computed Values From Other Computed Values - Tic-Tac-Toe Game: The “Game Over” Screen & Checking for a Draw - Why Immutability Matters - Always - When NOT To Lift State Up - An Alternative To Lifting State Up - Final Polishing & Improving Components

--- 4 React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 4 React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,37 +1,29 @@
-import { useState } from 'react';
+export default function GameBoard({ onSelectSquare, board }) {
+   // THIS PATTERN OF DEALING WITH MULTIPLE STATES SHOULD BE AVOID, KEEPING THIS FOR REFERENCE
+   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-const initialGameBoard = [
-   [null, null, null],
-   [null, null, null],
-   [null, null, null],
-];
+   // function handleSelectSquare(rowIndex, colIndex) {
+   //    setGameBoard((prevGameBoard) => {
+   //       const updatedBoard = [
+   //          ...prevGameBoard.map((innerArray) => [...innerArray]),
+   //       ];
+   //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+   //       return updatedBoard;
+   //    });
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-   function handleSelectSquare(rowIndex, colIndex) {
-      setGameBoard((prevGameBoard) => {
-         const updatedBoard = [
-            ...prevGameBoard.map((innerArray) => [...innerArray]),
-         ];
-         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-         return updatedBoard;
-      });
-
-      onSelectSquare();
-   }
+   //    onSelectSquare();
+   // }
 
    return (
       <ol id="game-board">
-         {gameBoard.map((row, rowIndex) => (
+         {board.map((row, rowIndex) => (
             <li key={rowIndex}>
                <ol>
                   {row.map((playerSymbol, colIndex) => (
                      <li key={colIndex}>
                         <button
-                           onClick={() =>
-                              handleSelectSquare(rowIndex, colIndex)
-                           }
+                           onClick={() => onSelectSquare(rowIndex, colIndex)}
+                           disabled={playerSymbol !== null}
                         >
                            {playerSymbol}
                         </button>
