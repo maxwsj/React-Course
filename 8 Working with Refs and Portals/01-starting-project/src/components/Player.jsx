2f17f6c35fc296b6803a69a4d49576c1175e8622
maxwsj <mw2study@gmail.com>
2025-02-05 18:44:09 -0300

    forwarding refst to custom components

--- 8 Working with Refs and Portals/01-starting-project/src/components/Player.jsx
+++ 8 Working with Refs and Portals/01-starting-project/src/components/Player.jsx
@@ -12,7 +12,9 @@ export default function Player() {
 
    return (
       <section id="player">
-         <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+         <h2>
+            Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}
+         </h2>
          <p>
             <input type="text" ref={playerName} />
             <button onClick={handleClick}>Set Name</button>
