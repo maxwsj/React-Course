b2f583ec24a2f89e9d408bebd85995f6f2bc1c11
maxwsj <mw2study@gmail.com>
2025-02-06 17:22:09 -0300

    handling project deletion

--- 9 Practice Project - Project Management App ( with Components, State, Refs and More )/01-starting-project/src/App.jsx
+++ 9 Practice Project - Project Management App ( with Components, State, Refs and More )/01-starting-project/src/App.jsx
@@ -52,11 +52,29 @@ function App() {
       });
    }
 
+   function handleDeleteProject() {
+      setProjectsState((prevState) => {
+         return {
+            ...prevState,
+            selectedProjectId: undefined,
+            // used to delet the item
+            projects: prevState.projects.filter(
+               (project) => project.id !== prevState.selectedProjectId
+            ),
+         };
+      });
+   }
+
    const selectedProject = projectsState.projects.find(
       (project) => project.id === projectsState.selectedProjectId
    );
 
-   let content = <SelectedProject project={selectedProject} />;
+   let content = (
+      <SelectedProject
+         project={selectedProject}
+         onDelete={handleDeleteProject}
+      />
+   );
 
    if (projectsState.selectedProjectId === null) {
       content = (
