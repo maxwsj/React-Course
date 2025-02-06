f4f13356a4c6ca2028240e297c5209912b49a6f1
maxwsj <mw2study@gmail.com>
2025-02-06 13:56:15 -0300

    adding a 'projects sidebar' component

---
+++ 9 Practice Project - Project Management App ( with Components, State, Refs and More )/01-starting-project/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+   return (
+      <aside>
+         <h2>Your Projects</h2>
+         <div>
+            <buttton>+ Add Project</buttton>
+         </div>
+         <ul></ul>
+      </aside>
+   );
+}
