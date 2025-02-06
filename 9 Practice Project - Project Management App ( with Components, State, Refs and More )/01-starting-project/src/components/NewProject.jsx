9cec3751f2e8e4247a855d9a4cb0dcf3d87df69f
maxwsj <mw2study@gmail.com>
2025-02-06 14:08:55 -0300

    Styling theSidebar & Button with Tailwind CSS Adding the “New Project” Component & A Reusable “Input” Component

---
+++ 9 Practice Project - Project Management App ( with Components, State, Refs and More )/01-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,19 @@
+import Input from './Input';
+
+export default function NewProject() {
+   return (
+      <div>
+         <menu>
+            <li>
+               <button>Cancel</button>
+               <button>Save</button>
+            </li>
+         </menu>
+         <div>
+            <Input label="Title" />
+            <Input label="Description" textarea />
+            <Input label="Due Date" />
+         </div>
+      </div>
+   );
+}
