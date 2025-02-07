18cbaa12b71ced312bf47c8664c6181bbdcad4ce
maxwsj <mw2study@gmail.com>
2025-02-07 00:21:17 -0300

    migrating the entire demo project to use the Context API

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Cart.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Cart.jsx
@@ -2,8 +2,8 @@ import { useContext } from 'react';
 // import { use } from 'react';
 import { CartContext } from '../store/shopping-cart-context';
 
-export default function Cart({ onUpdateItemQuantity }) {
-   const { items } = useContext(CartContext);
+export default function Cart() {
+   const { items, updateItemQuantity } = useContext(CartContext);
 
    const totalPrice = items.reduce(
       (acc, item) => acc + item.price * item.quantity,
@@ -27,13 +27,13 @@ export default function Cart({ onUpdateItemQuantity }) {
                         </div>
                         <div className="cart-item-actions">
                            <button
-                              onClick={() => onUpdateItemQuantity(item.id, -1)}
+                              onClick={() => updateItemQuantity(item.id, -1)}
                            >
                               -
                            </button>
                            <span>{item.quantity}</span>
                            <button
-                              onClick={() => onUpdateItemQuantity(item.id, 1)}
+                              onClick={() => updateItemQuantity(item.id, 1)}
                            >
                               +
                            </button>
--- 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Header.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Header.jsx
@@ -1,45 +1,44 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
-  const modal = useRef();
+export default function Header() {
+   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+   const { items } = useContext(CartContext);
 
-  function handleOpenCartClick() {
-    modal.current.open();
-  }
+   const cartQuantity = items.length;
 
-  let modalActions = <button>Close</button>;
+   function handleOpenCartClick() {
+      modal.current.open();
+   }
 
-  if (cartQuantity > 0) {
-    modalActions = (
+   let modalActions = <button>Close</button>;
+
+   if (cartQuantity > 0) {
+      modalActions = (
+         <>
+            <button>Close</button>
+            <button>Checkout</button>
+         </>
+      );
+   }
+
+   return (
       <>
-        <button>Close</button>
-        <button>Checkout</button>
+         <CartModal ref={modal} title="Your Cart" actions={modalActions} />
+         <header id="main-header">
+            <div id="main-title">
+               <img src="logo.png" alt="Elegant model" />
+               <h1>Elegant Context</h1>
+            </div>
+            <p>
+               <button onClick={handleOpenCartClick}>
+                  Cart ({cartQuantity})
+               </button>
+            </p>
+         </header>
       </>
-    );
-  }
-
-  return (
-    <>
-      <CartModal
-        ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
-        title="Your Cart"
-        actions={modalActions}
-      />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
-      </header>
-    </>
-  );
+   );
 }
