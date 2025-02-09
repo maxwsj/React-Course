bc7e16f51d794b8cd5a4ba07bf663cbb34101bfe
maxwsj <mw2study@gmail.com>
2025-02-08 22:58:26 -0300

    using useEffect for syncing with browser APIs

--- 11 Handling Side Effects and Working with the useEffect() Hook/01-starting-project/src/components/Modal.jsx
+++ 11 Handling Side Effects and Working with the useEffect() Hook/01-starting-project/src/components/Modal.jsx
@@ -1,8 +1,16 @@
-import { forwardRef, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = forwardRef(function Modal({ open, children }) {
    const dialog = useRef();
+
+   useEffect(() => {
+      if (open) {
+         dialog.current.showModal();
+      } else {
+         dialog.current.close();
+      }
+   }, []);
 
    return createPortal(
       <dialog className="modal" ref={dialog}>
