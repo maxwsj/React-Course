a62224c9106634e037dd30f6994e7cffdd564a09
maxwsj <mw2study@gmail.com>
2025-02-10 23:38:24 -0300

    handling HTTP errors

--- 15 Sending Http Requests (connecting to a database)/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15 Sending Http Requests (connecting to a database)/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,21 +1,41 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
    const [isFecthing, setIsFecthing] = useState(false);
    const [availablePlaces, setAvailablePlaces] = useState([]);
+   const [error, setError] = useState();
 
    useEffect(() => {
       async function fetchPlaces() {
          setIsFecthing(true);
-         const response = await fetch('http://localhost:3000/places');
-         const resData = await response.json();
-         setAvailablePlaces(resData.places);
+
+         try {
+            const response = await fetch('http://localhost:3000/places');
+            const resData = await response.json();
+
+            if (!response.ok) {
+               throw new Error('Failed to fetch places');
+            }
+
+            setAvailablePlaces(resData.places);
+         } catch (error) {
+            setError({
+               message:
+                  error.message ||
+                  'Could not fetch places, please try again later',
+            });
+         }
          setIsFecthing(false);
       }
 
       fetchPlaces();
    }, []);
+
+   if (error) {
+      return <Error title="An error occurre!" message={error.message} />;
+   }
 
    return (
       <Places
