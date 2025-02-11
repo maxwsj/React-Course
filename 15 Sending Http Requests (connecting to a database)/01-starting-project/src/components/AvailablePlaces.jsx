034cf096f5353078f3072e5e093a7fa11fe1ceaf
maxwsj <mw2study@gmail.com>
2025-02-10 23:15:35 -0300

    add some concepts of htpp request: - sending HTTP requests (GET request) via useEffect - using async / await - handling loading states

--- 15 Sending Http Requests (connecting to a database)/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15 Sending Http Requests (connecting to a database)/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+   const [isFecthing, setIsFecthing] = useState(false);
    const [availablePlaces, setAvailablePlaces] = useState([]);
+
+   useEffect(() => {
+      async function fetchPlaces() {
+         setIsFecthing(true);
+         const response = await fetch('http://localhost:3000/places');
+         const resData = await response.json();
+         setAvailablePlaces(resData.places);
+         setIsFecthing(false);
+      }
+
+      fetchPlaces();
+   }, []);
 
    return (
       <Places
          title="Available Places"
          places={availablePlaces}
+         isLoading={isFecthing}
+         loadingText="Fecthing place data..."
          fallbackText="No places available."
          onSelectPlace={onSelectPlace}
       />
