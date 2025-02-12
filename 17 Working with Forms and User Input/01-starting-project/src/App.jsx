43d1abf63ec938faa6296df2edb5c3fa5cda6020
maxwsj <mw2study@gmail.com>
2025-02-12 00:08:53 -0300

    validating input upon form submission

--- 17 Working with Forms and User Input/01-starting-project/src/App.jsx
+++ 17 Working with Forms and User Input/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
-import Login from './components/StateLogin.jsx';
-import Header from './components/Header.jsx';
+// import Login from './components/StateLogin.jsx';
 // import Signup from './components/Signup.jsx';
+import Login from './components/Login.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
    return (
