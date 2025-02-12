f5519a2d8d27b70e5ece1167ab6f6d8b519270a7
maxwsj <mw2study@gmail.com>
2025-02-12 14:18:50 -0300

    add validation checks

--- 18 Handling Forms via Form Actions/01-starting-project/src/components/Signup.jsx
+++ 18 Handling Forms via Form Actions/01-starting-project/src/components/Signup.jsx
@@ -15,6 +15,38 @@ export default function Signup() {
       const role = formData.get('role');
       const terms = formData.get('terms');
       const acquisitionChannel = formData.getAll('acquisition');
+
+      let errors = [];
+
+      if (!isEmail(email)) {
+         errors.push('Invalid email address.');
+      }
+
+      if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+         errors.push(
+            'You must provide a password with at least six characters.'
+         );
+      }
+
+      if (!isEqualToOtherValue(password, confirmPassword)) {
+         errors.push('Passwords do not match.');
+      }
+
+      if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+         errors.push('Please provide both your first and last name.');
+      }
+
+      if (!isNotEmpty(role)) {
+         errors.push('Please select a role.');
+      }
+
+      if (!terms) {
+         errors.push('You must agree to the terms and conditions.');
+      }
+
+      if (acquisitionChannel.legth === 0) {
+         errors.push('Please select at least one acquisition channel.');
+      }
    }
 
    return (
