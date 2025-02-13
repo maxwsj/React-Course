9608b40aa9361140072d04b3029d8e86bcc08937
maxwsj <mw2study@gmail.com>
2025-02-12 22:05:15 -0300

    using the 'pending' state from useActionState

--- 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/Opinion.jsx
+++ 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import { use } from 'react';
+import { use, useActionState } from 'react';
 import { OpinionsContext } from '../store/opinions-context';
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
@@ -11,6 +11,12 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       await downvoteOpinion(id);
    }
 
+   const [upvoteFormState, upvoteFormAction, upvotePending] =
+      useActionState(upvoteAction);
+
+   const [downvoteFormState, downvoteFormAction, downvotePending] =
+      useActionState(downvoteAction);
+
    return (
       <article>
          <header>
@@ -19,7 +25,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
          </header>
          <p>{body}</p>
          <form className="votes">
-            <button formAction={upvoteAction}>
+            <button
+               formAction={upvoteFormAction}
+               disabled={upvotePending || downvotePending}
+            >
                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -39,7 +48,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
             <span>{votes}</span>
 
-            <button formAction={downvoteAction}>
+            <button
+               formAction={downvoteFormAction}
+               disabled={upvotePending || downvotePending}
+            >
                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
