a94d14179e88d228b6fd6d0bb78020967a4de488
maxwsj <mw2study@gmail.com>
2025-02-12 21:06:15 -0300

    working with asynchronous form actions

--- 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/NewOpinion.jsx
+++ 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/NewOpinion.jsx
@@ -1,7 +1,10 @@
-import { useActionState } from 'react';
+import { useActionState, use } from 'react';
+import { OpinionsContext } from '../store/opinions-context';
 
 export function NewOpinion() {
-   function shareOpinionAction(prevState, formData) {
+   const { addOpinion } = use(OpinionsContext);
+
+   async function shareOpinionAction(prevState, formData) {
       const title = formData.get('title');
       const body = formData.get('body');
       const userName = formData.get('userName');
@@ -32,7 +35,7 @@ export function NewOpinion() {
       }
 
       // submit to backend
-
+      await addOpinion({ title, body, userName });
       return { errors: null };
    }
 
--- 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/Opinions.jsx
+++ 18 Handling Forms via Form Actions/06-adv-starting-project/src/components/Opinions.jsx
@@ -4,23 +4,23 @@ import { Opinion } from './Opinion';
 import { OpinionsContext } from '../store/opinions-context';
 
 export function Opinions() {
-  const { opinions } = use(OpinionsContext);
-  
-  return (
-    <div id="opinions">
-      <h2>User Opinions</h2>
-      {opinions && (
-        <ul>
-          {opinions.map((o) => (
-            <li key={o.id}>
-              <Opinion opinion={o} />
-            </li>
-          ))}
-        </ul>
-      )}
-      {!opinions && (
-        <p>No opinions found. Maybe share your opinion on something?</p>
-      )}
-    </div>
-  );
+   const { opinions } = use(OpinionsContext);
+
+   return (
+      <div id="opinions">
+         <h2>User Opinions</h2>
+         {opinions && (
+            <ul>
+               {opinions.map((o) => (
+                  <li key={o.id}>
+                     <Opinion opinion={o} />
+                  </li>
+               ))}
+            </ul>
+         )}
+         {!opinions && (
+            <p>No opinions found. Maybe share your opinion on something?</p>
+         )}
+      </div>
+   );
 }
