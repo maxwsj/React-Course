3ce8ce203bcee24c0858985ee20aa7426a72198f
maxwsj <mw2study@gmail.com>
2025-02-13 19:01:00 -0300

    handling form submission and validation

--- 19 Practice Project - Building a Food Order App/01-starting-project/src/App.jsx
+++ 19 Practice Project - Building a Food Order App/01-starting-project/src/App.jsx
@@ -4,6 +4,7 @@ import Cart from './components/Cart';
 
 import { CartContextProvider } from './store/CartContext';
 import { UserProgressContextProvider } from './store/UserProgressContext';
+import Checkout from './components/Checkout';
 
 function App() {
    return (
@@ -12,6 +13,7 @@ function App() {
             <Header />
             <Meals />
             <Cart />
+            <Checkout />
          </CartContextProvider>
       </UserProgressContextProvider>
    );
