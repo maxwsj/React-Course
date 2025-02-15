ba2e2e92ac60dffc3969498b150d5922f832aaba
maxwsj <mw2study@gmail.com>
2025-02-14 21:01:26 -0300

    dispatching actions from inside components

--- 20 Diving into Redux (An Alternative to The Context API)/01-starting-project/src/components/Counter.js
+++ 20 Diving into Redux (An Alternative to The Context API)/01-starting-project/src/components/Counter.js
@@ -1,9 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-   // We call this and now we need to pass a function to use selector. A function which will be executed by a React Redux, a function which will then basically determines which piece of data we wanna extract from our store.
+   const dispatch = useDispatch();
    const counter = useSelector((state) => state.counter);
+
+   const incrementHandler = () => {
+      dispatch({ type: 'increment' });
+   };
+
+   const decrementHandler = () => {
+      dispatch({ type: 'decrement' });
+   };
 
    const toggleCounterHandler = () => {};
 
@@ -11,6 +19,10 @@ const Counter = () => {
       <main className={classes.counter}>
          <h1>Redux Counter</h1>
          <div className={classes.value}>{counter}</div>
+         <div>
+            <button onClick={decrementHandler}>Decrement</button>
+            <button onClick={incrementHandler}>Increment</button>
+         </div>
          <button onClick={toggleCounterHandler}>Toggle Counter</button>
       </main>
    );
