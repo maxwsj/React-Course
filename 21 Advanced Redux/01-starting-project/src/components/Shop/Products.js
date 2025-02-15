85edc481e13e3ac18e51f6d50f63e2de09ca6790
maxwsj <mw2study@gmail.com>
2025-02-15 17:44:57 -0300

    add more redux refresher practice part 2

--- 21 Advanced Redux/01-starting-project/src/components/Shop/Products.js
+++ 21 Advanced Redux/01-starting-project/src/components/Shop/Products.js
@@ -1,19 +1,24 @@
+import DUMMY_PRODUCTS from '../../util/dummy-data';
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+const Products = () => {
+   return (
+      <section className={classes.products}>
+         <h2>Buy your favorite products</h2>
+         <ul>
+            {DUMMY_PRODUCTS.map((product) => (
+               <ProductItem
+                  title={product.title}
+                  price={product.price}
+                  description={product.description}
+                  key={product.id}
+                  id={product.id}
+               />
+            ))}
+         </ul>
+      </section>
+   );
 };
 
 export default Products;
