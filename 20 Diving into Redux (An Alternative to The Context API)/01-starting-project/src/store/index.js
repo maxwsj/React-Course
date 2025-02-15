f01be455115097765eef2547ff72776eff4b1cf7
maxwsj <mw2study@gmail.com>
2025-02-14 22:14:27 -0300

    using redux toolkit and adding state slices

--- 20 Diving into Redux (An Alternative to The Context API)/01-starting-project/src/store/index.js
+++ 20 Diving into Redux (An Alternative to The Context API)/01-starting-project/src/store/index.js
@@ -1,9 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
 
 const initialState = {
    counter: 0,
    showCounter: true,
 };
+
+createSlice({
+   // Here we are preparing a slice of our global state
+   name: 'counter',
+   initialState, // its the object above
+   // Are an object / map of all recuers that this slice needs
+   reducer: {
+      increment(state) {
+         state.counter++;
+      },
+      decrement(state) {
+         state.counter--;
+      },
+      increase(state, action) {
+         state.counter = state.counter + action.amount;
+      },
+      toggleCounter(state) {
+         state.showCounter = !state.showCounter;
+      },
+   },
+});
 
 const counterReducer = (state = initialState, action) => {
    if (action.type === 'increment') {
