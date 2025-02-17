ce7b1e43451eb20049476df71b93edd17812f0b3
maxwsj <mw2study@gmail.com>
2025-02-16 21:18:14 -0300

    using useEffect with Redux to send HTTP request to Firebase

---
+++ 21 Advanced Redux/01-starting-project/src/util/dummy-data.js
@@ -0,0 +1,40 @@
+const DUMMY_PRODUCTS = [
+   {
+      id: 'p1',
+      price: 6,
+      title: 'My First Book',
+      description: 'The first book I ever wrote',
+   },
+   {
+      id: 'p2',
+      price: 12,
+      title: 'Adventures in Coding',
+      description: 'A fun guide to programming for beginners.',
+   },
+   {
+      id: 'p3',
+      price: 8,
+      title: 'The Mystery of the Lost Island',
+      description: 'A thrilling adventure novel set on a remote island.',
+   },
+   {
+      id: 'p4',
+      price: 15,
+      title: 'Mastering React',
+      description: 'An in-depth guide to building React applications.',
+   },
+   {
+      id: 'p5',
+      price: 10,
+      title: 'Space Explorers',
+      description: 'A sci-fi novel about the first human journey to Mars.',
+   },
+   {
+      id: 'p6',
+      price: 7,
+      title: 'The Art of Cooking',
+      description: 'A cookbook filled with delicious and easy recipes.',
+   },
+];
+
+export default DUMMY_PRODUCTS;
