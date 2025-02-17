f67b40716fc399e479cc5d8e125efdeb2cfa7da4
maxwsj <mw2study@gmail.com>
2025-02-17 19:47:07 -0300

    navigating programmatically

--- 22 Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Products.js
+++ 22 Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Products.js
@@ -1,3 +1,12 @@
 export default function Products() {
-   return <h1>The product page</h1>;
+   return (
+      <>
+         <h1>The product page</h1>
+         <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+         </ul>
+      </>
+   );
 }
