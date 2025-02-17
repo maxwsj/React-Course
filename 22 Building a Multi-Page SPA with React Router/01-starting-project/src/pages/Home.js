13e79f6b279bd3539449f29a376d93d503313e98
maxwsj <mw2study@gmail.com>
2025-02-17 17:01:53 -0300

    navigating programmatically

--- 22 Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Home.js
+++ 22 Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Home.js
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+   // We will get access to navigate programmatically
+   const navigation = useNavigate();
+
+   // We should use a link for navigation instead of this approach, this is just for learning purposes !!
+   function navigateHandler() {
+      navigation('/products');
+   }
+
    return (
       <>
          <h1>My Home Page</h1>
          <p>
             Go to <Link to="/products">the list of products</Link>
+         </p>
+         <p>
+            <button onClick={navigateHandler}>Navigate</button>
          </p>
       </>
    );
