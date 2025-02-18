5e553fc7e37448fea32dd03c594c7cb1b2491008
maxwsj <mw2study@gmail.com>
2025-02-18 20:51:05 -0300

    adding automatic logout

--- 23 Adding Authentication to React Apps/01-starting-project/frontend/src/pages/Root.js
+++ 23 Adding Authentication to React Apps/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
    // const navigation = useNavigation();
+   const token = useLoaderData();
+   const submit = useSubmit();
+
+   useEffect(() => {
+      if (!token) {
+         return;
+      }
+
+      setTimeout(() => {
+         submit(null, { action: '/logout', metod: 'post' });
+      }, 1 * 60 * 60 * 1000);
+
+      return () => {};
+   }, [token, submit]);
 
    return (
       <>
