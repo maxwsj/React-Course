2edb6e9fb30d56590bda20b08a59ed0c60150252
maxwsj <mw2study@gmail.com>
2025-02-18 23:50:57 -0300

    setting initial project and adding some deployment steps logic

---
+++ 24 Deploying React Apps/01-starting-project/src/App.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+// Importing a component so it can be loaded in a lazy loading way when it's needed
+const BlogPage = lazy(() => import('./pages/Blog'));
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+         {
+            index: true,
+            element: <HomePage />,
+         },
+         {
+            path: 'posts',
+            children: [
+               {
+                  index: true,
+                  // Suspense lets us put a fullback element until the children load
+                  element: (
+                     <Suspense fallback={<p>Loading...</p>}>
+                        <BlogPage />
+                     </Suspense>
+                  ),
+                  loader: () =>
+                     import('./pages/Blog').then((module) => module.loader()),
+               },
+               { path: ':id', element: <PostPage />, loader: postLoader },
+            ],
+         },
+      ],
+   },
+]);
+
+function App() {
+   return <RouterProvider router={router} />;
+}
+
+export default App;
