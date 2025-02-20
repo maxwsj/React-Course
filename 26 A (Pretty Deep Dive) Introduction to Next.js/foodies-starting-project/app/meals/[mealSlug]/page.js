24be44863285744869d73f06176da3bc600a5d9f
maxwsj <mw2study@gmail.com>
2025-02-20 18:47:38 -0300

    loading and rendering meal details via dynamcic routes and route parameters

--- 26 A (Pretty Deep Dive) Introduction to Next.js/foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 26 A (Pretty Deep Dive) Introduction to Next.js/foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -1,8 +1,32 @@
-export default function MealDetailsPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({ params }) {
+   const meal = getMeal(params.mealSlug);
+
+   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
    return (
       <>
-         <header></header>
-         <main></main>
+         <header className={classes.header}>
+            <div className={classes.image}>
+               <Image fill src={meal.image} alt={meal.title} />
+            </div>
+            <div className={classes.headerText}>
+               <h1>{meal.title}</h1>
+               <p className={classes.creator}>
+                  by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+               </p>
+               <p className={classes.summary}>{meal.summary}</p>
+            </div>
+         </header>
+         <main className={''}>
+            <p
+               className={classes.instructions}
+               dangerouslySetInnerHTML={{ __html: meal.instructions }}
+            ></p>
+         </main>
       </>
    );
 }
--- 26 A (Pretty Deep Dive) Introduction to Next.js/foodies-starting-project/lib/meals.js
+++ 26 A (Pretty Deep Dive) Introduction to Next.js/foodies-starting-project/lib/meals.js
@@ -6,3 +6,8 @@ export async function getMeals() {
    await new Promise((resolve) => setTimeout(resolve, 2000));
    return db.prepare('SELECT * FROM meals').all();
 }
+
+export function getMeal(slug) {
+   // This kind of structure prevents us from SQL injection
+   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
