8e4a611a512b55cb9734ec93e98fb5f85a25cfc3
maxwsj <mw2study@gmail.com>
2025-02-21 18:29:01 -0300

    adding 'head' metadata

--- 26 A (Pretty Deep Dive) Introduction to Next.js/onwards-to-a-bigger/pages/[meetupId]/index.js
+++ 26 A (Pretty Deep Dive) Introduction to Next.js/onwards-to-a-bigger/pages/[meetupId]/index.js
@@ -2,14 +2,20 @@ import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails(props) {
+export default function MeetupDetails(props) {
    return (
-      <MeetupDetail
-         image={props.meetupData.image}
-         title={props.meetupData.title}
-         address={props.meetupData.address}
-         description={props.meetupData.description}
-      />
+      <Fragment>
+         <Head>
+            <title>{props.meetupData.title}</title>
+            <meta name="description" content={props.meetupData.description} />
+         </Head>
+         <MeetupDetail
+            image={props.meetupData.image}
+            title={props.meetupData.title}
+            address={props.meetupData.address}
+            description={props.meetupData.description}
+         />
+      </Fragment>
    );
 }
 
@@ -63,5 +69,3 @@ export async function getStaticProps(context) {
       },
    };
 }
-
-export default MeetupDetails;
