9cb96426ac426c02bed337d6e27836c040770305
maxwsj <mw2study@gmail.com>
2025-02-22 20:33:39 -0300

    add more frame motion animation concepts: - adding entry animations - animating elements disappearances and removal - making elements "Pop" with hover animations

--- 28 Animating React Apps/01-starting-project/src/components/Modal.jsx
+++ 28 Animating React Apps/01-starting-project/src/components/Modal.jsx
@@ -1,14 +1,30 @@
+import { motion } from 'framer-motion';
 import { createPortal } from 'react-dom';
 
 export default function Modal({ title, children, onClose }) {
-  return createPortal(
-    <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
-        <h2>{title}</h2>
-        {children}
-      </dialog>
-    </>,
-    document.getElementById('modal')
-  );
+   return createPortal(
+      <>
+         <div className="backdrop" onClick={onClose} />
+         <motion.dialog
+            initial={{
+               opacity: 0,
+               y: 30,
+            }} // allow us to define an initial state for the to be performed animations, that will be assumed immediately after this element has been added to the DOM
+            animate={{
+               opacity: 1,
+               y: 0,
+            }}
+            exit={{
+               opacity: 0,
+               y: 30,
+            }}
+            open
+            className="modal"
+         >
+            <h2>{title}</h2>
+            {children}
+         </motion.dialog>
+      </>,
+      document.getElementById('modal')
+   );
 }
