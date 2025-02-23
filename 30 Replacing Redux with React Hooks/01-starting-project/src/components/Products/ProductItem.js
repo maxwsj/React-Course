0dc030d461e461a295db680a837f2cfc4e990338
maxwsj <mw2study@gmail.com>
2025-02-23 17:10:29 -0300

    optimizing the custom hook store

--- 30 Replacing Redux with React Hooks/01-starting-project/src/components/Products/ProductItem.js
+++ 30 Replacing Redux with React Hooks/01-starting-project/src/components/Products/ProductItem.js
@@ -6,7 +6,7 @@ import './ProductItem.css';
 import { useStore } from '../../hooks-store/store';
 
 const ProductItem = (props) => {
-   const dispatch = useStore()[1];
+   const dispatch = useStore(false)[1];
 
    const toggleFavHandler = () => {
       dispatch('TOGGLE_FAV', props.id);
