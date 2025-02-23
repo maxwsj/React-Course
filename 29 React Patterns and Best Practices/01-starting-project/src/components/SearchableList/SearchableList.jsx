b6f63c3a265f98c790584fe9661301e9df8e5114
maxwsj <mw2study@gmail.com>
2025-02-23 14:39:54 -0300

    working with deboucing

--- 29 React Patterns and Best Practices/01-starting-project/src/components/SearchableList/SearchableList.jsx
+++ 29 React Patterns and Best Practices/01-starting-project/src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+   const lastChange = useRef();
    const [searchTerm, setSearchTerm] = useState('');
 
    const searchResults = items.filter((item) =>
@@ -8,12 +9,24 @@ export default function SearchableList({ items, itemKeyFn, children }) {
    );
 
    function handleChange(event) {
-      setSearchTerm(event.target.value);
+      if (lastChange.current) {
+         clearTimeout(lastChange.current);
+      }
+
+      lastChange.current = setTimeout(() => {
+         lastChange.current = null;
+         setSearchTerm(event.target.value);
+      }, 1000);
    }
 
    return (
       <div className="searchable-list">
-         <input type="search" placeholder="Search" onChange={handleChange} />
+         <input
+            type="search"
+            placeholder="Search"
+            onChange={handleChange}
+            ref={lastChange}
+         />
          <ul>
             {searchResults.map((item) => (
                <li key={itemKeyFn(item)}>{children(item)}</li>
