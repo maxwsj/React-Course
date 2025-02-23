d72a23c3874a96539a0c253d981547fe6ee67e30
maxwsj <mw2study@gmail.com>
2025-02-23 16:01:47 -0300

    setting initial project and add a context

---
+++ 30 Replacing Redux with React Hooks/01-starting-project/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import ProductsProvider from './context/products-context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <ProductsProvider>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </ProductsProvider>
+);
