b5ee695d9dd122f12c7f14c2b9f5cef00b1cb484
maxwsj <mw2study@gmail.com>
2025-02-24 00:05:58 -0300

    working with types: Numbers, Strings, Booleans, Array, Object

--- 32 React + TypeScript/01-starting-project/basics.ts
+++ 32 React + TypeScript/01-starting-project/basics.ts
@@ -4,7 +4,33 @@
 
 // Primitives
 let age: number;
-age = 12
+age = 12;
 
 let userName: string;
 let isInstructor: boolean;
+
+// More complex types
+let hobbies: string[];
+let cars: number[];
+let choices: boolean[];
+
+hobbies = ['Teste', 'Teste2'];
+cars = [1, 2, 3];
+choices = [true, false, false];
+
+// Objects
+let person: {
+   name: string;
+   age: number;
+};
+
+person = {
+   name: 'Max',
+   age: 32,
+};
+
+// Creating an array of objects
+let people: {
+   name: string;
+   age: number;
+}[];
