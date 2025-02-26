e55797fd6f946841646445a82e79dca0017d3bb6
maxwsj <mw2study@gmail.com>
2025-02-25 22:55:58 -0300

    managing state and typescript

--- 32 React + TypeScript/07-react-ts-starting-project/src/components/NewTodo.tsx
+++ 32 React + TypeScript/07-react-ts-starting-project/src/components/NewTodo.tsx
@@ -1,11 +1,26 @@
-const NewTodo = () => {
+import { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+   const todoTextInputRef = useRef<HTMLInputElement>(null);
+
    const submitHandler = (event: React.FormEvent) => {
       event.preventDefault();
+
+      const enteredtext = todoTextInputRef.current!.value;
+
+      if (enteredtext.trim().length === 0) {
+         // throw an error
+         return;
+      }
+
+      props.onAddTodo(enteredtext);
    };
 
    return (
       <form onSubmit={submitHandler}>
-         <label htmlFor="text">Todo text</label>
+         <label htmlFor="text" ref={todoTextInputRef}>
+            Todo text
+         </label>
          <input type="text" id="text" />
          <button>Add Todo</button>
       </form>
